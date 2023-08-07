ef16632622c488c92e7e9e2dd3dd307772678427
haramsong <alakazam1324@gmail.com>
2023-08-07 17:14:15 +0900

    first commit

---
+++ layout/default.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="layout">
+		<SideNav />
+		<div class="content">
+			<HeaderTab></HeaderTab>
+			<main>
+				<slot />
+			</main>
+			<FooterTab></FooterTab>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	components: {
+		HeaderTab,
+		FooterTab,
+	},
+	layout: 'default',
+};
+</script>
+
+<style></style>
