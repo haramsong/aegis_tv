4b97365723aa5d4995215b585c3b557d9c0716ae
haramsong <alakazam1324@gmail.com>
2023-08-07 17:51:41 +0900

    layout 적용

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="layout">
+		<CommonSideNav />
+		<div class="content">
+			<CommonHeader />
+			<slot />
+			<CommonFooter />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
