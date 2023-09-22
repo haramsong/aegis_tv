8bc1cd9a90ff8a096f069d8d8d6a2f8fba4b62d9
haramsong <alakazam1324@gmail.com>
2023-09-22 15:00:24 +0900

    설정파일 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-quasar-ui', 'nuxt-vercel-analytics'],
+  modules: ['nuxt-quasar-ui'],
   css: ['@/assets/scss/main.scss'],
   quasar: {
     lang: 'ko-KR',
